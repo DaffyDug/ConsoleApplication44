bb98a3cfa810a62c71e42e6f940c746db12cc5c4
Александр Александр <stripalol@inbox.ru>
2024-03-05 11:34:48 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication44/ConsoleApplication44.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void doublex(double* x)
+{
+	*x = (rand() % 10 + 1) / 10.0;
+}
+
+void RandomArr(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 50;
+		cout << arr[i] << "\t";
+	}
+}
+
+int main()
+{
+	srand(time(0));
+	double* x = new double();
+	doublex(x);
+	cout << *x << "\n\n";
+	int* arr = new int[10];
+	RandomArr(arr, 10);
+}
